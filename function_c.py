b7d0af25f1a6298b900fb061bce9c09100cff4ab
Cristal <cbcodee@gmail.com>
2022-09-23 14:01:56 -0700

    <COMMIT MESSAGE>

--- function_c.py
+++ function_c.py
@@ -3,8 +3,4 @@ def merge_lists(list_a, list_b):
         a combination of list_a and list_b
         without any duplicate elements.
     """
-    pass
-
-
-if __name__ == "__main__":
-    print(merge_lists([1, 1, 2, 3], [3, 4, 5]))
+    return list(set(list_a) | set(list_b))
