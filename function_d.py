79b956a738648ac7fd00c4ca694469e775a04bea
Tanil <cadette.tanil@gmail.com>
2022-09-23 17:57:49 -0400

    added comment to function d

--- function_d.py
+++ function_d.py
@@ -4,6 +4,6 @@ def max_value(numbers):
     """
     max_number = max(numbers)
     return max_number
-
+# testing out adding lines
 if __name__ == "__main__":
     print(max_value([1, 12, 2, 42, 8, 3]))
